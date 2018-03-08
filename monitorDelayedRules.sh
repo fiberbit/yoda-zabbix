ae850025e5f45074d698a82123f2222a107b565d
Hans <j.w.kleinloog@uu.nl>
2018-03-08 13:34:16 +0100

    replaced all .sh files with linux versions (made with vi)

---
+++ monitorDelayedRules.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# \file		monitorDelayedRules.sh
+# \brief	Call rule to count the actual delayed rules for monitoring.
+# \copyright	Copyright (c) 2018, Utrecht University. All rights reserved.
+
+sudo -u irods irule -F /etc/irods/yoda-zabbix/monitorDelayedRules.r
