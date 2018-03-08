5a3d3d195690518dd53b2fd27bf1da88ba62b767
hanskleinloog <30900358+hanskleinloog@users.noreply.github.com>
2018-03-08 13:47:37 +0100

    editorial

--- testUserParam.sh
+++ testUserParam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # \file		testUserParam.sh
-# \brief	for testpurposes. zabbix key with parameter (yoda.test.userparam[x]
+# \brief	for testpurposes. zabbix key with parameter (yoda.test.userparam[x])
 # \copyright	Copyright (c) 2018, Utrecht University. All rights reserved.
 
 echo $1
