b97435e1420153f5145b0e802b277ec496cd26c0
Hans <j.w.kleinloog@uu.nl>
2018-03-16 14:56:03 +0100

    replaced test content with actual code

--- dailyRodslogErrors.sh
+++ dailyRodslogErrors.sh
@@ -1,1 +1,14 @@
-echo 2
+#!/bin/bash
+# \author		Niek Bats
+# \file			dailyRodslogErrors.sh
+# \brief		Count number of current errors of the current day found in the rodsLog
+# \copyright	Copyright (c) 2018, Utrecht University. All rights reserved.
+#
+#gets latest log file
+filepaths=$(sudo -u irods ls /var/lib/irods/iRODS/server/log/rodsLog.* | tail -n 2)
+
+#gets current month and day
+monthDay=$(date +"%b %d")
+
+#gets lines containing error and month day
+echo $(sudo -u irods grep "$monthDay" $filepaths | grep "ERROR" | wc -l)
--- hourlyRodslogErrors.sh
+++ hourlyRodslogErrors.sh
@@ -1,1 +1,14 @@
-echo 3
+#!/bin/bash
+# \author		Niek Bats
+# \file			hourlyRodslogErrors.sh
+# \brief		Count number of current errors of the current hour found in the rodsLog
+# \copyright	Copyright (c) 2018, Utrecht University. All rights reserved.
+#
+#gets latest log file
+filepaths=$(sudo -u irods ls /var/lib/irods/iRODS/server/log/rodsLog.* | tail -n 2)
+
+#gets current month day and hour
+monthDayHour=$(date +"%b %d %H")
+
+#gets lines containing error and month day hour
+echo $(sudo -u irods grep "$monthDayHour" $filepaths | grep "ERROR" | wc -l)
