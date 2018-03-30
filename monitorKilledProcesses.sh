1bd37f5efb60b59248251e4a7fe7163ab3e3dca2
Hans <j.w.kleinloog@uu.nl>
2018-03-30 16:51:30 +0200

    added text

--- monitorKilledProcesses.sh
+++ monitorKilledProcesses.sh
@@ -3,6 +3,7 @@
 # \file      mostKilledProcesses.sh
 # \brief     Returns the number of lines in /var/log/messages.for killed processes due to lack of virutal memory of the current day
 #.\brief     Mar 29 09:29:50 combined kernel: Killed process 15639 (irodsServer) total-vm:913264kB, anon-rss:754528kB, file-rss:4kB, shmem-rss:22556kB
+#.\          Initial version
 # \copyright Copyright (c) 2018, Utrecht University. All rights reserved.
 
 # Gets current month and day.
