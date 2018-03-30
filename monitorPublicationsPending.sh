efeedc070f132f162d3b74e9781b7d799f5fb33d
Hans <j.w.kleinloog@uu.nl>
2018-03-30 17:17:32 +0200

    converted windows to linux, tabs to spaces

--- monitorPublicationsPending.sh
+++ monitorPublicationsPending.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# \file		monitorPublicationsPending.sh
-# \brief	Call rule to monitor pending applications.
-# \		The count of publications that are pending longer than the number of seconds,
+# \file     monitorPublicationsPending.sh
+# \brief    Call rule to monitor pending applications.
+# \     The count of publications that are pending longer than the number of seconds,
 # \             specified in the zabbix item key parameter is returned 
-# \copyright	Copyright (c) 2018, Utrecht University. All rights reserved.
+# \copyright    Copyright (c) 2018, Utrecht University. All rights reserved.
 
 sudo -u irods irule -F /etc/irods/yoda-zabbix/monitorPublicationsPending.r $1
