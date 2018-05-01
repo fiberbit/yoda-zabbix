984e5996d3609c1e82e642a4588a63bb2528c1c4
niekbats <niek.bats@triplearch.eu>
2018-05-01 12:54:57 +0200

    hopefully without awkward performance
    remade with awk

---
+++ replicationErrors.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# \author       Niek Bats
+# \file         replicationErrors.sh
+# \brief        Returns number of replication errors from the last 60minutes
+# \copyright    Copyright (c) 2018, Utrecht University. All rights reserved.
+
+#gets the two latest rodslog files
+filepaths=$(ls /var/lib/irods/log/rodsLog.* | tail -n 2)
+#echo "$filepaths"
+
+echo "$(awk -v d1="$(date --date="-60 min" "+%b %e %H:%M")" -v d2="$(date "+%b %e %H:%M")" '$0 > d1 && $0 < d2 || $0 ~ d2' $filepaths | grep -cE "uuReplicate|msiDataObjRepl")"
