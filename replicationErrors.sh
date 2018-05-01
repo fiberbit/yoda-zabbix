7fa57807a8ce4dc6723aab3a2a11087cb27cd822
niekbats <niek.bats@triplearch.eu>
2018-05-01 15:56:12 +0200

    added sudo for ls

--- replicationErrors.sh
+++ replicationErrors.sh
@@ -5,6 +5,6 @@
 # \copyright    Copyright (c) 2018, Utrecht University. All rights reserved.
 
 #gets the two latest rodslog files
-filepaths=$(ls /var/lib/irods/log/rodsLog.* | tail -n 2)
+filepaths=$(sudo -u irods ls /var/lib/irods/log/rodsLog.* | tail -n 2)
 
 echo "$(awk -v d1="$(date --date="-60 min" "+%b %e %H:%M")" -v d2="$(date "+%b %e %H:%M")" '$0 > d1 && $0 < d2 || $0 ~ d2' $filepaths | grep -cE "uuReplicate|msiDataObjRepl")"
