662c30226af3fbab708a91fc4975f4db9dfaa2c5
Hans <j.w.kleinloog@uu.nl>
2019-01-21 11:32:53 +0100

    renamed irodsconnectioncheck to irodsservicecheck

--- irodsServiceCheck.sh
+++ irodsServiceCheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# \file         irodsConnectionCheck.sh
+# \file         irodsServiceCheck.sh
 # \brief        Check irods connection (port 1247) by requesting basic server information.
 # \author       Hans Kleinloog
 # \copyright    Copyright (c) 2019, Utrecht University. All rights reserved.
