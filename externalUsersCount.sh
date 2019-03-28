2e421179a487d94705cc53e4a8648e130adbb971
Hans <j.w.kleinloog@uu.nl>
2019-03-28 14:02:14 +0100

    changed selection of internalusers to uu.nl so that students.uu.nl is also counted as internaluser. Changed externalusers accordingly

--- externalUsersCount.sh
+++ externalUsersCount.sh
@@ -4,5 +4,5 @@
 # \brief        Count number of external Yoda users.
 # \copyright    Copyright (c) 2018, Utrecht University. All rights reserved.
 
-totalExternalUsers=$(sudo -u irods iadmin lu | grep -v @uu.nl | wc -l)
+totalExternalUsers=$(sudo -u irods iadmin lu | grep -v uu.nl | wc -l)
 echo $totalExternalUsers
--- internalUsersCount.sh
+++ internalUsersCount.sh
@@ -4,5 +4,5 @@
 # \brief	Count number of internal Yoda users.
 # \copyright	Copyright (c) 2018, Utrecht University. All rights reserved.
 
-totalInternalUsers=$(sudo -u irods iadmin lu | grep @uu.nl | wc -l)
+totalInternalUsers=$(sudo -u irods iadmin lu | grep uu.nl | wc -l)
 echo $totalInternalUsers
