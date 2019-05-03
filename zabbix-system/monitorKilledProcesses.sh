7f0b0c39d385f83f37d4c9d9da5a0654aeaa5c14
hanskleinloog <j.w.kleinloog@uu.nl>
2019-05-03 12:40:24 +0200

    cleaned comment lines

--- zabbix-system/monitorKilledProcesses.sh
+++ zabbix-system/monitorKilledProcesses.sh
@@ -2,8 +2,8 @@
 
 # \file      monitorKilledProcesses.sh
 # \brief     Returns the number of lines in /var/log/messages.for killed processes due to lack of virtual memory of the current day
-#.\brief     Mar 29 09:29:50 combined kernel: Killed process 15639 (irodsServer) total-vm:913264kB, anon-rss:754528kB, file-rss:4kB, shmem-rss:22556kB
-#.\          Initial version
+# \brief     Mar 29 09:29:50 combined kernel: Killed process 15639 (irodsServer) total-vm:913264kB, anon-rss:754528kB, file-rss:4kB, shmem-rss:22556kB
+# \          Initial version
 # \copyright Copyright (c) 2018, Utrecht University. All rights reserved.
 
 #gets current month day
