28b9738ccf540c30bf4098dd00c145c0221a9dfc
Sietse Snel <s.t.snel@uu.nl>
2023-01-27 11:03:37 +0100

    YDA-5033: WebDAV user checks: adjust for fmt type
    Adjust column to retrieve user names from in log file if X-Forwarded-For headers
    are logged in Apache.

--- zabbix-portal/dailyWebdavUsers.sh
+++ zabbix-portal/dailyWebdavUsers.sh
@@ -7,5 +7,12 @@
 # gets latest log file.
 filepath=/var/log/httpd/webdav_access.log
 
-#counts unique webdav users in the last day
-/etc/zabbix/zabbix_agentd.d/read-apache-logs.py --last day "$filepath" | grep '/ HTTP/1.1" 20' | awk '{print $3}' | sort | uniq | wc -l
+# Check logformat
+grep -q "LogFormat.*X-Forwarded-For.*common$" /etc/httpd/conf/httpd.conf
+logf_enabled="$?"
+
+# counts unique webdav users in the past hour
+if [ $logf_enabled == "0" ]
+then /etc/zabbix/zabbix_agentd.d/read-apache-logs.py --last day "$filepath" | grep '/ HTTP/1.1" 20' | awk '{print $4}' | sort | uniq | wc -l
+else /etc/zabbix/zabbix_agentd.d/read-apache-logs.py --last day "$filepath" | grep '/ HTTP/1.1" 20' | awk '{print $3}' | sort | uniq | wc -l
+fi
--- zabbix-portal/hourlyWebdavUsers.sh
+++ zabbix-portal/hourlyWebdavUsers.sh
@@ -7,5 +7,12 @@
 # gets latest log file.
 filepath=/var/log/httpd/webdav_access.log
 
+# Check logformat
+grep -q "LogFormat.*X-Forwarded-For.*common$" /etc/httpd/conf/httpd.conf
+logf_enabled="$?"
+
 # counts unique webdav users in the past hour
-/etc/zabbix/zabbix_agentd.d/read-apache-logs.py --last hour "$filepath" | grep '/ HTTP/1.1" 20' | awk '{print $3}' | sort | uniq | wc -l
+if [ $logf_enabled == "0" ]
+then /etc/zabbix/zabbix_agentd.d/read-apache-logs.py --last hour "$filepath" | grep '/ HTTP/1.1" 20' | awk '{print $4}' | sort | uniq | wc -l
+else /etc/zabbix/zabbix_agentd.d/read-apache-logs.py --last hour "$filepath" | grep '/ HTTP/1.1" 20' | awk '{print $3}' | sort | uniq | wc -l
+fi
